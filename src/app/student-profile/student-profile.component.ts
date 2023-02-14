89c14598a43706d001f9475ba291611bd241bf1e
Rusnak Volodymyr <vrusna@softserveinc.com>
2023-02-14 20:11:00 +0200

    add subjects to student profile and fix lastname

--- src/app/student-profile/student-profile.component.ts
+++ src/app/student-profile/student-profile.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {StudentProfileService} from "../shared/services/student-profile.service";
 import {StudentProfile} from "../shared/models/student-profile.model";
+import {MasterSubject} from "../shared/models/subject.model";
+import {StudyGroupService} from "../shared/services/study-group.service";
 
 @Component({
   selector: 'app-student-profile',
@@ -10,13 +12,21 @@ import {StudentProfile} from "../shared/models/student-profile.model";
 export class StudentProfileComponent implements OnInit {
 
   student?: StudentProfile;
+  subjects: MasterSubject[];
 
-  constructor(private studentService: StudentProfileService) {
+  constructor(private studentService: StudentProfileService, private studyGroupService: StudyGroupService) {
   }
 
   ngOnInit(): void {
     this.studentService.getProfile().subscribe(response => {
       this.student = response;
+      this.retrieveStudyGroupSubjects(this.student.studyGroups[0].id);
+    })
+  }
+
+  retrieveStudyGroupSubjects(studyGroupId: number){
+    this.studyGroupService.getGroupSubjects(studyGroupId).subscribe(response => {
+      this.subjects = response;
     })
   }
 
