9c7fbfe6beffa10e29ec8dd1104294f66a8ee621
Rusnak Volodymyr <vrusna@softserveinc.com>
2023-02-15 12:06:07 +0200

    save darkmode progress

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,7 +1,9 @@
-import {Component, EventEmitter, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {LayoutService} from "../../shared/services/layout.service";
 import {MenuItemModel} from "../../shared/models/menu.model";
 import {MenuService} from "../../shared/services/menu.service";
+import {ThemeService} from "../../shared/services/theme.service";
+import {Theme} from "../../shared/enums/theme";
 
 @Component({
   selector: 'app-sidebar',
@@ -12,8 +14,9 @@ export class SidebarComponent implements OnInit {
 
   display: boolean = false;
   menuItems: MenuItemModel[];
+  isDarkMode: boolean = false;
 
-  constructor(private layoutService: LayoutService, private menuService: MenuService) {
+  constructor(private layoutService: LayoutService, private menuService: MenuService, private themeService: ThemeService) {
     layoutService.menuToggled.subscribe(sidebarState => this.display = sidebarState);
     menuService.menuUpdated.subscribe(response => this.menuItems = response);
   }
@@ -26,5 +29,10 @@ export class SidebarComponent implements OnInit {
 
   closeSidebar() {
     this.layoutService.menuToggled.emit(false);
+  }
+
+  switchTheme() {
+    let newMode = this.isDarkMode ? Theme.DarkMode : Theme.LightMode;
+    this.themeService.switchTheme(newMode);
   }
 }
---
+++ src/app/shared/enums/theme.ts
@@ -0,0 +1,4 @@
+export enum Theme {
+  LightMode = 'lara-light-blue',
+  DarkMode = 'lara-dark-purple'
+};
