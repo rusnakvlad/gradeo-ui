ef727073e9de4fb836b09940a135ae1c078d4f1d
Rusnak Volodymyr <vrusna@softserveinc.com>
2023-02-15 10:16:01 +0200

    add real statistics data on student profile page

--- src/app/student-profile/student-profile.component.ts
+++ src/app/student-profile/student-profile.component.ts
@@ -5,6 +5,9 @@ import {MasterSubject} from "../shared/models/subject.model";
 import {StudyGroupService} from "../shared/services/study-group.service";
 import {ChartModule} from "primeng/chart";
 import {colors} from "../shared/models/colors.model";
+import {StatisticsService} from "../shared/services/statistics.service";
+import {SpinnerService} from "../shared/services/spinner.service";
+import {BarChartModel} from "../shared/models/chart.model";
 
 @Component({
   selector: 'app-student-profile',
@@ -18,8 +21,12 @@ export class StudentProfileComponent implements OnInit {
 
   chartData: any;
   chartOptions: any;
+  averageGrades: BarChartModel[];
 
-  constructor(private studentService: StudentProfileService, private studyGroupService: StudyGroupService) {
+  constructor(private studentService: StudentProfileService,
+              private studyGroupService: StudyGroupService,
+              private statisticsService: StatisticsService,
+              private spinner: SpinnerService) {
   }
 
   ngOnInit(): void {
@@ -27,6 +34,7 @@ export class StudentProfileComponent implements OnInit {
       this.student = response;
       this.retrieveStudyGroupSubjects(this.student.studyGroups[0].id);
     })
+    this.retrieveAverageGrades();
   }
 
   retrieveStudyGroupSubjects(studyGroupId: number) {
@@ -36,14 +44,23 @@ export class StudentProfileComponent implements OnInit {
     })
   }
 
+  retrieveAverageGrades(){
+    this.spinner.show();
+    this.statisticsService.getCurrentStudentAverageGrades().subscribe(response => {
+    this.averageGrades = response;
+    this.buildChart();
+    this.spinner.hide();
+    })
+  }
+
   buildChart() {
     this.chartData = {
-      labels: this.subjects.map(x => x.name),
+      labels: this.averageGrades.map(x => x.label),
       datasets: [
         {
           label: 'Average grade per subject',
           backgroundColor: colors,
-          data: [65, 59, 80, 81, 56, 55]
+          data: this.averageGrades.map(x => x.value)
         }
       ]
     };
